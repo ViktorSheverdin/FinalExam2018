c9c2bbdcbf77f69820afb1e9ddd1c09104eb3e9a
ViktorSheverdin <viktor.sheverdin@gmail.com>
2018-04-15 12:26:33 -0700

    Final Exam init

--- main.js
+++ main.js
@@ -1,5 +1,19 @@
 const express = require('express');
 const hbs = require('hbs');
-const fs = require('fs');
-const geo = require('./weather.js')
+const fs = require('fs')
 var app = express();
+
+const port = process.env.PORT || 8080;
+
+app.set('view engine', 'hbs');
+hbs.registerPartials(__dirname + '/views/partials');
+
+
+app.get("/home", (request, response) => {
+    response.render('index.hbs');
+});
+
+
+app.listen(port, () => {
+    console.log('Server is up on the port 8080');
+});
